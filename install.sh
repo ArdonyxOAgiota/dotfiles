6746ca80bb48c3c4074baebc586de3b2d7572afd
Ardonyx <ardonyx@arch.localdomain>
2022-01-07 20:00:24 +0000

    atualizando o install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 bash -c "mkdir ~/.config/alacritty && mkdir ~/.config/nvim"
+bash -c " curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 bash -c "cp /alacritty/alacritty.yml ~/.config/alacritty && cp /nvim/init.vim ~/.config/nvim"
