e41d768bed951a3651a57da2adab4283b206f572
ArdonyxOAgiota <97315222+ArdonyxOAgiota@users.noreply.github.com>
2022-01-08 00:09:05 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+while true; do
+	read -p "Você tem certeza que quer executar o script?(Digite s ou n) " -r sn
+    case "${sn}" in
+        [Ss]* )
+          printf "OK"
+          break;;
+        [Nn]* ) exit;;
+        * ) echo "";;
+    esac
+done
 bash -c "mkdir ~/.config/alacritty && mkdir ~/.config/nvim"
 bash -c " curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 bash -c "cp alacritty/alacritty.yml ~/.config/alacritty && cp nvim/init.vim ~/.config/nvim"
